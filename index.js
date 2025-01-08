c70ee7070b1923b6b0c4303b9e14c08ddbcff8e5
jess-icaa <jessicashalominselvan@JESSICAs-Laptop.local>
2025-01-08 18:53:15 +0530

    committed

--- index.js
+++ index.js
@@ -1,13 +1,52 @@
 const express = require('express');
 const { resolve } = require('path');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const User = require('./schema');
+dotenv.config();
 
 const app = express();
 const port = 3010;
 
 app.use(express.static('static'));
+app.use(bodyParser.json());
+
+mongoose
+.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+.then(() => console.log('Connected to database'))
+.catch((err) => console.error('Error connecting to database:', err.message));
 
 app.get('/', (req, res) => {
   res.sendFile(resolve(__dirname, 'pages/index.html'));
+});
+
+app.post('/api/users', async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    if (!name || !email || !password) {
+      return res.status(400).json({ message: 'Validation error : All fields are required'});
+    }
+
+    const newUser = new User({ name, email, password });
+    await newUser.save();
+    res.status(201).json({ message: 'User created successfully' });
+  } catch (err) {
+    if (err.code === 11000) {
+      return res.status(400).json({ message: 'Validation error: Email already exists' });
+    }
+    res.status(500).json({ message: 'Server error', error: err.message });
+  }
+});
+
+app.get('/api/users', async (req, res) => {
+  try {
+    const users = await User.find();
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json({ message: 'Server error', error: err.message });
+  }
 });
 
 app.listen(port, () => {
